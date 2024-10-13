2b1ce2ff07a9609a498c6282d1616356d785c992
unknown <tako1201.44.5@gmail.com>
2024-10-13 12:31:03 +0800

    delete not used

--- frontend/src/app/app-routing.ts
+++ frontend/src/app/app-routing.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotDevelopedComponent } from './not-developed/not-developed.component';
-import { InvoiceDetailComponent } from './invoice-detail/invoice-detail.component';
-import { EinvoiceDetailComponent } from './einvoice-detail/einvoice-detail.component';
+//import { InvoiceDetailComponent } from './invoice-detail/invoice-detail.component';
+//import { EinvoiceDetailComponent } from './einvoice-detail/einvoice-detail.component';
 import { EinvoiceComponent } from './einvoice/einvoice.component';
 import { EinvoiceLoginComponent } from './einvoice-login/einvoice-login.component';
 
@@ -10,8 +10,8 @@ export const routes: Routes = [
   { path: '', redirectTo: 'einvoiceLogin', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'not-developed', component: NotDevelopedComponent },
-  { path: 'invoiceDetail', component: InvoiceDetailComponent },
-  { path: 'einvoiceDetail', component: EinvoiceDetailComponent },
+  //{ path: 'invoiceDetail', component: InvoiceDetailComponent },
+  //{ path: 'einvoiceDetail', component: EinvoiceDetailComponent },
   { path: 'einvoice', component: EinvoiceComponent },
   { path: 'einvoiceLogin', component: EinvoiceLoginComponent }
 ];
